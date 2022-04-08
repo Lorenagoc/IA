16d384f4b92f99a3161a80b4d4e5bf2fefc6cade
Lorenagoc <lorenagoc99@gmail.com>
2022-04-08 19:48:24 -0300

    Updating code but still not done

--- A-star.py
+++ A-star.py
@@ -1,88 +1,137 @@
-from collections import defaultdict
-from typing import List, Dict
+from collections import deque
 
-class Cidade:
-	def __init__(self, heuristica):
-		self.heuristica = heuristica
-		self.adjacencias = {}
-	
-	def insere(self, vizinho, custo:int):
-		self.adjacencias[vizinho] = custo
-
-	def obtem_heuristica(self):
-		return self.heuristica
 
 class Grafo:
-	
-	def __init__(self):
-		self.cidades = {}
-		self.grafo = defaultdict(list) #criamos esse atributo para fazer um dicionario de listas e facilitar a implementacao
 
-	def adiciona_cidade(self, nome, heuristica_cidade) -> Cidade:
-		nova_cidade = Cidade(heuristica_cidade)
-		self.cidades[nome] = nova_cidade
-		return nova_cidade
+    # Cria o objeto grafo que possui vizinhos como atributos
+    def __init__(self, vertices) -> None:
+        self.vertices = vertices
 
-	def adiciona_custo(self, cidade_origem, cidade_destino, custo):
-		self.grafo[cidade_origem].append(cidade_destino) #cria uma aresta ligando um vertice u e um vertice v
-		vertice_origem = self.obtem_cidade(cidade_origem)
-		vertice_destino = self.obtem_cidade(cidade_destino)
-		if not vertice_origem is None and not vertice_destino is None:
-			vertice_origem.insere(vertice_destino, custo)
+    # Método para retornar os vizinhos do nó
+    def obtem_vizinhos(self, node):
+        return self.vertices[node]
 
-	def obtem_cidade(self, nome_cidade:str) -> Cidade:
-		if nome_cidade in self.cidades:
-			return self.cidades[nome_cidade]
-		else:
-			return None
+    # Tabela de heurísticas das cidades
+    def h(self, node):
+        H = {
+            'Arad': 366,
+            'Bucareste': 0,
+            'Craiova': 160,
+            'Drobeta': 242,
+            'Eforie': 161,
+            'Fagaras': 176,
+            'Giurgiu': 77,
+            'Hirsova': 151,
+            'Iasi': 226,
+            'Lugoj': 244,
+            'Mehadia': 241,
+            'Neamt': 234,
+            'Oradea': 380,
+            'Pitesti': 100,
+            'Rimnicu Vilcea': 193,
+            'Sibiu': 253,
+            'Timisoara': 329,
+            'Urziceni': 80,
+            'Vaslui': 199,
+            'Zerind': 374
+        }
 
-	def vizinhos(self, nodo):
-		return self.grafo[nodo] #retorna o vizinho desse nodo especifico
+        return H[node]
+
+    # Implementação do algoritm A* sendo f(n) = g(n) + h(n)
+    def a_star(self, origem, destino):
+
+        # Nós que foram abertos começando da cidade de origem (vizinhos não visitados)
+        lista_abertos = set([origem])
+        # Nós que foram fechados (vizinhos já visitados)
+        lista_fechados = set([])
+
+        g = {}  # Custo do caminho do nó inicial até o nó n
+        g[origem] = 0
+
+        visitados = {}  # Mapa de vizinhos visitados
+        visitados[origem] = origem
+
+        # Enquanto ainda houver nós abertos com vizinhos não visitados...
+        while len(lista_abertos) > 0:
+
+            n = None  # Nó com menor f(n) até o momento
+
+            # Calcula menor custo do caminho total estimado
+            for vertice_atual in lista_abertos:
+                if n == None or g[vertice_atual] + self.h(vertice_atual) < g[n] + self.h(n):
+                    n = vertice_atual
+
+            if n == None:
+                print('O caminho não existe')
+                return None
+
+            # Se chegamos na cidade destino então percorre o caminho de volta
+            if n == destino:
+                caminho = []
+
+                # Enquanto o nó houver vértices já visitados
+                while visitados[n] != n:
+                    # Adiciona no array todos os nós que foram abertos e verificados
+                    caminho.append(n)
+                    n = visitados[n]  # Passa para o próximo
+
+                caminho.append(origem)  # Terminando, adiciona o nó de origem
+                caminho.reverse()  # Inverte todo o array
+
+                print('Melhor caminho: {}'.format(caminho))
+                return caminho
+
+            # Para todos os vizinhos do nó atual
+            for(vizinho, h_vizinho) in self.obtem_vizinhos(n):
+                # Se o vertice ainda não foi aberto nem teve seus vizinhos inspecionados
+                if vizinho not in lista_abertos and vizinho not in lista_fechados:
+                    lista_abertos.add(vizinho)  # Adiciona na lista de abertos
+                    # Adiciona como um vértice visitado de n
+                    visitados[vizinho] = n
+                    # Calcula o custo do caminho até o vertice em questão
+                    g[vizinho] = g[n] + h_vizinho
+                # Se o vertice não foi aberto ou já foi aberto mas não inspecionado
+                else:
+                    # Verifica se compensa seguir em frente
+                    if g[vizinho] > g[n] + h_vizinho:
+                        # Atualiza o custo do vizinho (adiciona o valor acumulativo no lugar)
+                        g[vizinho] = g[n] + h_vizinho
+                        # Adiciona como um vértice visitado de n
+                        visitados[vizinho] = n
+
+                        if vizinho in lista_fechados:
+                            lista_fechados.remove(vizinho)
+                            lista_abertos.add(vizinho)
+
+            lista_abertos.remove(n)
+            lista_fechados.add(n)
+
+        print('O caminho não existe')
+        return None
+
 
 if __name__ == "__main__":
 
-	mapa = Grafo()
+    mapa = {
+        'Oradea': [('Zerind', 71), ('Sibiu', 151)],
+        'Zerind': [('Arad', 75)],
+        'Arad': [('Sibiu', 140), ('Timisoara', 118)],
+        'Timisoara': [('Lugoj', 111)],
+        'Lugoj': [('Mehadia', 70)],
+        'Mehadia': [('Drobeta', 75)],
+        'Drobeta': [('Craiova', 120)],
+        'Craiova': [('Rimnicu Vilcea', 146), ('Pitesti', 138)],
+        'Rimnicu Vilcea': [('Pitesti', 97), ('Sibiu', 80)],
+        'Sibiu': [('Fagaras', 99)],
+        'Fagaras': [('Bucareste', 211)],
+        'Pitesti': [('Bucareste', 101)],
+        'Bucareste': [('Giurgiu', 90), ('Urziceni', 85)],
+        'Urziceni': [('Hirsova', 98), ('Vaslui', 142)],
+        'Hirsova': [('Eforie', 86)],
+        'Vaslui': [('Iasi', 92)],
+        'Iasi': [('Neamt', 87)]
+    }
 
-	mapa.adiciona_cidade("Arad", 366)
-	mapa.adiciona_cidade("Bucareste", 0)
-	mapa.adiciona_cidade("Craiova", 160)
-	mapa.adiciona_cidade("Drobeta", 242)
-	mapa.adiciona_cidade("Eforie", 161)
-	mapa.adiciona_cidade("Fagaras", 176)
-	mapa.adiciona_cidade("Giurgiu", 77)
-	mapa.adiciona_cidade("Hirsova", 151)
-	mapa.adiciona_cidade("Iasi", 226)
-	mapa.adiciona_cidade("Lugoj", 244)
-	mapa.adiciona_cidade("Mehadia", 241)
-	mapa.adiciona_cidade("Neamt", 234)
-	mapa.adiciona_cidade("Oradea", 380)
-	mapa.adiciona_cidade("Pitesti", 100)
-	mapa.adiciona_cidade("Rimnicu Vilcea", 193)
-	mapa.adiciona_cidade("Sibiu", 253)
-	mapa.adiciona_cidade("Timisoara", 329)
-	mapa.adiciona_cidade("Urziceni", 80)
-	mapa.adiciona_cidade("Vaslui", 199)
-	mapa.adiciona_cidade("Zerind", 374)
-
-	mapa.adiciona_custo("Arad","Sibiu", 140)
-	mapa.adiciona_custo("Arad","Zerind", 75)
-	mapa.adiciona_custo("Arad", "Timisoara", 118)
-	mapa.adiciona_custo("Zerind", "Oradea", 71)
-	mapa.adiciona_custo("Oradea", "Sibiu", 151)
-	mapa.adiciona_custo("Timisoara", "Lugoj", 111)
-	mapa.adiciona_custo("Lugoj", "Mehadia", 70)
-	mapa.adiciona_custo("Mehadia", "Drobeta", 75)
-	mapa.adiciona_custo("Drobeta", "Craiova", 120)
-	mapa.adiciona_custo("Craiova", "Pitesti", 138)
-	mapa.adiciona_custo("Craiova", "Rimnicu Vilcea", 146)
-	mapa.adiciona_custo("Rimnicu Vilcea", "Sibiu", 80)
-	mapa.adiciona_custo("Rimnicu Vilcea", "Pitesti", 101)
-	mapa.adiciona_custo("Sibiu", "Fagaras", 99)
-	mapa.adiciona_custo("Fagaras", "Bucareste", 211)
-	mapa.adiciona_custo("Bucareste", "Giurgiu", 90)
-	mapa.adiciona_custo("Bucareste", "Urzicene", 85)
-	mapa.adiciona_custo("Urzicene", "Hirsova", 98)
-	mapa.adiciona_custo("Urzicene", "Vaslui", 142)
-	mapa.adiciona_custo("Vaslui", "Iasi", 92)
-	mapa.adiciona_custo("Iasi", "Neamt", 87)
-	mapa.adiciona_custo("Hirsova", "Eforie", 86)
+    grafo = Grafo(mapa)
+    grafo.a_star('Arad', 'Bucareste')
